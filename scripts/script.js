c72a3bcc9912b37a2b5ae1d3d301adf817d52489
shame <shame.bdao@gmail.com>
2021-12-26 21:40:25 +1100

    Added playRound function to simulate a game

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,37 @@ function computerPlay() {
             return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    // rock vs. paper => you lose
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock";
+    } 
+
+    // rock vs. scissors => you win
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors";
+    }
+    // paper vs. rock => you win
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock";
+    }
+    // paper vs. scissors => you lose
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper";
+    }
+    // scissors vs. rock => you lose
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors";
+    }
+    // scissors vs. paper => you win
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beat Paper";
+    }
+    else {
+        return `It's a tie! You both picked ${playerSelection[0].toUpperCase() + playerSelection.slice(1)}`;
+    }
+}
